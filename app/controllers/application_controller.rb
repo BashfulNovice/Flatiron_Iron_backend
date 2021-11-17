a61500d2f3cf450f8eb094cc03b5f4117ec29043
Todd Krieger <twkriege@mtu.edu>
2021-11-17 11:23:15 -0500

    post, get, delete endpoints made

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < Sinatra::Base
     { message: "Good luck with your project!" }.to_json
   end
 
+  #Workout Routs
   get "/workouts" do
     Workout.all.map{ |wo| {workout: wo, exerciese: wo.selected_exercises}}.to_json
   end
@@ -15,4 +16,49 @@ class ApplicationController < Sinatra::Base
     exs = Workout.find(params[:id]).selected_exercises
     {workout: workout, exercieses: exs}.to_json
   end
+
+  delete "workouts/:id" do
+    workout = Workout.find(params[:id])
+    workout.destroy
+    workout.to_json
+  end
+
+  post "/workouts" do
+    workout = Workout.create(
+      name: params[:name],
+      date: params[:date],
+      rest: params[:rest]
+    )
+    workout.to_json
+  end
+
+# Selected exercise routs
+
+  get "/selectedexercises" do
+    SelectedExercise.all.to_json
+  end
+
+  get "/selctedexercises/:id" do
+    SelectedExercise.find(params[:id]).to_json
+  end
+
+  delete "/selctedexercises/:id" do
+    ex = SelectedExercise.find(params[:id])
+    ex.destroy
+    ex.to_json
+  end
+
+  post "/selectedexercises" do
+    ex = SelectedExercise.create(
+      name: params[:name],
+      sets: params[:sets],
+      reps: params[:reps],
+      workout_id: params[:workout_id],
+      exercise_id: params[:exercise_id]
+    )
+    ex.to_json
+  end
+
+
+
 end
